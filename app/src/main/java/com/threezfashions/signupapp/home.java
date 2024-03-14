e1b5f3b14bb08903b296adfbe8da695a330780bc
Nadeem Ansari <nadeem.nadeem@gmail.com>
2024-03-14 20:41:47 +0530

    New changes

--- app/src/main/java/com/threezfashions/signupapp/home.java
+++ app/src/main/java/com/threezfashions/signupapp/home.java
@@ -1,21 +1,30 @@
 package com.threezfashions.signupapp;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class home extends AppCompatActivity {
-
+ public DatabaseReference dbr;
     private EditText ed1, ed2, ed3, ed4, ed5;
     private Button button, button2;
+    private TextView Data_text_view;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +37,7 @@ public class home extends AppCompatActivity {
         ed5 = findViewById(R.id.ed5);
         button = findViewById(R.id.button);
         button2 = findViewById(R.id.button2);
+        Data_text_view = findViewById(R.id.Data_text_view);
 
         //Click button 1 to upload data to Firebase-Database
 
@@ -36,18 +46,35 @@ public class home extends AppCompatActivity {
           public void onClick(View v) {
               // Write a message to the database
               FirebaseDatabase database = FirebaseDatabase.getInstance();
-              DatabaseReference myRef = database.getReference("Storage");
+               dbr = database.getReference("Storage");
                  //
-              myRef.setValue("Name : " + ed1 + "Email : " + ed2 + "Phone : " + ed3 + "Password : " + ed4 + "description : " + ed5);
+              dbr.setValue("Name : " + ed1 + "Email : " + ed2 + "Phone : " + ed3 + "Password : " + ed4 + "description : " + ed5);
           }
       });
-      //Click button 2 to Go data Activity
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(home.this , data.class);
-                startActivity(intent);
-            }
-        });
+
+      button2.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+              // Read from the database
+              dbr.addValueEventListener(new ValueEventListener() {
+                  @Override
+                  public void onDataChange(DataSnapshot dataSnapshot) {
+                      // This method is called once with the initial value and again
+                      // whenever data at this location is updated.
+                      String value = dataSnapshot.getValue(String.class);
+                      Log.d(TAG, "This is Data :  " + Data_text_view);
+                  }
+
+
+
+                  @Override
+                  public void onCancelled(DatabaseError error) {
+                      // Failed to read value
+                      Log.w(TAG, "Failed to read value.", error.toException());
+                  }
+              });
+          }
+      });
+
     }
 }
