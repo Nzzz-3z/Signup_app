87415711999a5d30dbc7e0fa57d224c16ad107f7
Nadeem Ansari <nadeem.nadeem@gmail.com>
2024-03-14 18:11:53 +0530

    New changes

--- app/src/main/java/com/threezfashions/signupapp/home.java
+++ app/src/main/java/com/threezfashions/signupapp/home.java
@@ -2,23 +2,32 @@ package com.threezfashions.signupapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class home extends AppCompatActivity {
 
-
+    private EditText ed1, ed2, ed3, ed4, ed5;
+    private Button button, button2;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ed1 = findViewById(R.id.ed1);
+        ed2 = findViewById(R.id.ed2);
+        ed3 = findViewById(R.id.ed3);
+        ed4 = findViewById(R.id.ed4);
+        ed5 = findViewById(R.id.ed5);
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
 
-        myRef.setValue("Hello, World!");
+        myRef.setValue("s");
     }
 }
